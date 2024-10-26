ea157c025d51793f16b8ef7414fad26861e449ad
mostafa <abbas.mustapha@gmail.com>
2024-10-26 22:02:05 +0200

    22-24 Loading data in H2 and ResponseEntity GetById

--- src/main/java/com/mab/ecom_project/controller/ProductController.java
+++ src/main/java/com/mab/ecom_project/controller/ProductController.java
@@ -1,15 +1,38 @@
 package com.mab.ecom_project.controller;
 
+import com.mab.ecom_project.model.Product;
+import com.mab.ecom_project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
-    @RequestMapping("/")
-    public String greeting(){
-        return "Hello world from ProductController !";
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.I_AM_A_TEAPOT);
     }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer id){
+        Product product = service.getProductById(id);
+
+        if (product!=null)
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+insert into product (id,available, brand, category, desc , name, price , quantity )
+values (1,true,'renault','automobile','renault espacel','czero',9000,12);
+
+insert into product (id,available, brand, category, desc , name, price , quantity )
+values (2,false,'fiat','automobile','fiat 500','500',9002,12);
+
+insert into product (id,available, brand, category, desc , name, price , quantity )
+values (3,true,'honda','automobile','concerto','Concerto',9003,12);
+
+insert into product (id,available, brand, category, desc , name, price , quantity )
+values (4,false,'nissan','automobile','jsaispas','primera',9004,12);
+
+insert into product (id,available, brand, category, desc , name, price , quantity ,release_date)
+values (5,true,'toyota','automobile','corolla','yaris',9005,12,now());
+
+
